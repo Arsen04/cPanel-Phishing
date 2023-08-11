65a40e8286f35cb64937cd6492da8d0eaf6dd223
Arsen M <44641762+Arsen04@users.noreply.github.com>
2023-08-11 16:20:32 +0400

    Update action.php

--- src/phishingData/action.php
+++ src/phishingData/action.php
@@ -13,14 +13,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $conn -> real_escape_string($_POST['email']);
     $password = $conn -> real_escape_string($_POST['password']);
 
-    /*
-    $userID = 123;
-    $token = "some_generated_token";
-
-    $expiration = time() + (30 * 24 * 60 * 60);
-    setcookie('remember_me', $userID . ':' . $token, $expiration, '/');
-*/
-
     if ($user->loginUser($username, $password)) {
         $_SESSION['log']=true;
         echo "<script>window.location.href='dashboard.php'</script>";
